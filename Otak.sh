33a45e8934bde1c265bd7abee75281a366b88433
ahdad <78063619+ahdad@users.noreply.github.com>
2021-01-29 10:49:48 +0700

    Otak.sh

---
+++ Otak.sh
@@ -0,0 +1,174 @@
+read -p '| | | | ____| |   | |   / _ \
+| |_| |  _| | |   | |  | | | |
+|  _  | |___| |___| |__| |_| |
+|_| |_|_____|_____|_____\___/
+' userinput1;
+#
+if [ "${userinput1:-}" = "" ]
+then
+	clear
+fi
+echo -e "\e[34m┈┈┈┈▅┈┈┈┈┈▅ ┈┈
+┈┈┈▕┈┈┈╱╲▕▀┈┈┈
+┈┈┈╱╲┈┈▏▕╱╲┈┈┈
+┈┈┈▏▕╱╲▏▎▏▕╱╲┈▃
+┈╱╲▏▎▅▂▅▂▏▎▏▎▏▏
+▂▏▎▏▕╭┳┳╮▏┊▏▕╱╲
+▏▏┊▏▎┃┊┊┃▏▎▏▎▏▕
+▇▆▅▃▂┻┻┻┻▂▃▅▆▇▉\e[0m";
+echo -e "\e[91mHELLO SELAMAT DATANG DI TOOL BELAJAR HACK / HTML . SMD . JAVASCRIP . BAHASA PROGRAM\e[0m";
+echo -e "---------------------
+1.JAVASCRIP
+---------------------
+2.SMD
+---------------------
+3.BAHASA PROGRAM
+---------------------
+4.UNIX
+---------------------
+5.LINUX
+---------------------";
+read -p 'silakan pilih no berapa yang ingin anda pelajari dari kami :' userinput2;
+#
+if [ "${userinput2:-}" = "1" ]
+then
+	echo -e "\e[92m1.Pertama adalah javascript atau bisa disingkat dengan JS. Fungsi utama dari JS yaitu membuat suatu website lebih interaktif dan lebih menarik. JS adalah bahasa pemrograman yang script nya dijalankan di browser perangkat pengguna, berbeda dengan bahasa pemrograman lain yang scriptnya dijalankan oleh server.
+-------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "1" ]
+then
+	echo -e "\e[92mJavaScript adalah salah satu bahasa pemrograman yang paling banyak digunakan dalam kurun waktu dua puluh tahun ini. Bahkan JavaScript juga dikenal sebagai salah satu dari tiga bahasa pemrograman utama bagi web developer: HTML: Memungkinkan Anda untuk menambahkan konten ke halaman web.
+-------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "1" ]
+then
+	echo -e "\e[92mDi dalam bahasa pemrograman Javascript, tidak terdapat proses kompilasi lebih dahulu supaya program bisa Anda jalankan. Selain itu,aplikasi yang dibutuhkan adalah web browser dan text editor saja
+-------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "1" ]
+then
+	echo -e "\e[92mFramework JavaScript Terbaik
+
+Vue.Js. Vue.Js merupakan salah satuframework JavaScript yang juga bisa digunakan lintas platform. ...
+2. ReactJs. Framework JavaScript lainnya yang juga banyak digunakan adalah React Js
+3. AngularJs. Mungkin ini adalah salahframework JavaScript yang paling populer
+4. Angular Node.Js
+6. BackboneJs
+7. Ember. ...Meteor
+---------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "1" ]
+then
+	echo -e "\e[92mBrowser Android
+
+Buka browser bawaan Android.Pilih Menu.Pilih More lalu Settings.Pilih opsi Enable JavaScript.
+-------
+MUNGKIN HANYA SAMPAI SITU SAJA SAYA MENJELAS KAN NYA...\e[0m";
+fi
+if [ "${userinput2:-}" = "2" ]
+then
+	echo -e "\e[92m2.Command Prompt atau CMD adalah aplikasicommand line interpreter (CLI) yang ada di sistem operasi Windows. Perintah-perintah dalam CMD berfungsi untuk melakukan berbagai operasi di komputer Anda. Sebenarnya, perintah-perintah tersebut sudah ada sejak jaman MS-DOS.
+---------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "2" ]
+then
+	echo -e "\e[92mIni Dia 7 Perintah Command Prompt ParaHacker
+
+Ping. Perintah ping biasanya digunakan dalam menguji jaringan. ...
+Nslookup. Dari nslookup kita dapat mengetahui ip dari sebuah domain. ...
+Route. Perintah route berfungsi untuk melakukan routing pada komputer ke suatu jaringan LAN atau WAN. ...Tracert. ...ARP. ...Ipconfig. ...Netstat.
+---------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "2" ]
+then
+	echo -e "\e[92mMetode 1 : Menggunakan Run…
+
+1.Isikan dengan cmd dan tekan OK
+2.Maka akan muncul tampilan programCommand Prompt.
+3.Metode 2 : Melalui menu di Accessories.
+4.Jendela Command Prompt akan terbuka.
+Klik saja pada hasilnya cmd.exe untukmenjalankan Command Prompt.
+5.Klik menu Start, buka Accessories dan pilihCommand Prompt.
+---------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "2" ]
+then
+	echo -e "\e[92mApp belajar CMD
+1.Command Prompt
+2.Terminal Emulator for android
+---------
+MUNKIN HANYA ITU AJA YANG BISA SAYA BERI TAHU\e[0m";
+fi
+if [ "${userinput2:-}" = "3" ]
+then
+	echo -e "\e[92m3. Bahasa pemrograman, atau sering diistilahkan juga dengan bahasa komputer atau bahasa pemrograman komputer, adalah instruksi standar untuk memerintah komputer. Bahasa pemrograman ini merupakan suatu himpunan dari aturan sintaks dan semantik yang dipakai untuk mendefinisikan program komputer.
+----------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "3" ]
+then
+	echo -e "\e[92mBerikut beberapa jenis bahasa pemrogramanyang sangat populer dan sering digunakan oleh developer.
+
+Java. Menjadi salah satu bahasa yang sangat populer karena dapat digunakan pada berbagai platform yang berbeda.
+Bahasa C.
+PHP. 
+Visual Basic.
+Phyton.
+C++\e[0m";
+fi
+if [ "${userinput2:-}" = "3" ]
+then
+	echo -e "\e[92m------------------------------
+5 Aplikasi Terbaru untukBelajar Pemrograman Android
+
+1. Learn Java
+2. Codebox
+3. Udacity
+------------\e[0m";
+fi
+if [ "${userinput2:-}" = "3" ]
+then
+	echo -e "\e[31mMUNGKIN HANYA ITU YANG SAYA BISA JELAS KAN KARENA SAMA SAYA BATASI KALO TAU SEMUA NANTI ANDA JARANG MASUK KE TOOL INI WKWKWK...\e[0m";
+fi
+if [ "${userinput2:-}" = "4" ]
+then
+	echo -e "\e[92m4. Unix atau UNIX adalah sebuah sistem operasi komputer yang diawali dari project Multics pada tahun 1965 yang dilakukan American Telephone and Telegraph, General Electric, dan Institut Teknologi Massachusetts, dengan biaya dari Departemen Pertahanan Amerika
+---------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "4" ]
+then
+	echo -e "\e[92mPengertian UNIX adalah suatu sistem operasi yang memiliki model client-server yang mendukung untuk digunakan dalam jaringan computer karena keunggulannya yang dapat melakukan fungsi utamanya jaringan komputer lebih baik dari sistem operasi lain. Sistem operasi ini dikembangkan oleh satu bagian di
+---------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "4" ]
+then
+	echo -e "\e[92mPengertian dan fungsi linux adalah salah satu sistem operasi yang bermodel open source dan berbasis UNIX. Fungsi linux sendiri tidak jauh berbeda dengan sistem operasi lain yaitu melakukan multitasking, multiprogramming, multiprocessing dan time sharing
+---------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "4" ]
+then
+	echo -e "\e[92mAnda bisa mempelajari nya semua dengan mendownload app yang ada di google pley store
+Tulis saja sama anda app belajar UNIX
+------------
+Mungkin hanya itu aja yang bisa saya beritahu ke pada anda\e[0m";
+fi
+if [ "${userinput2:-}" = "5" ]
+then
+	echo -e "\e[92m5. Linux adalah keluarga sistem operasi bebas dan sumber terbuka yang dibangun di atas kernel Linux. Dengan lisensi bebas dan sumber terbukanya, Lisensi Publik Umum GNU, kode sumber Linux dapat dimodifikasi, digunakan dan didistribusikan kembali secara bebas oleh siapa saja. 
+--------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "5" ]
+then
+	echo -e "\e[92mLinux sendiri digunakan sebagai sistem operasi untuk berbagai jenis perangkat keras komputer seperti desktop, supercomputer, dan embedded sistem seperti buku pembaca elektronik, sistem permainan video (PlayStation 2, PlayStation 3, dan Xbox), telepon genggam dan router
+---------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "5" ]
+then
+	echo -e "\e[92mKekuatan dan fleksibilitas dari Linux inilah yang menjadikannya teman bermain bagihacker. Mereka menggunakan, mempelajari, dan memahaminya dengan sangat dalam. Alasan mendasar hacker menggunakan Linuxadalah karena kemampuan untuk melihat setiap baris kode Linux dan patch ketika masalah muncul.
+---------------------------\e[0m";
+fi
+if [ "${userinput2:-}" = "5" ]
+then
+	echo -e "\e[92mAnda bisa belajar lebih jauh lagi dengan mendownload app di pley store
+------------
+Hanya itu yang saya bisa jelas kan byeee\e[0m";
+fi
